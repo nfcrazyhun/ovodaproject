cfe3824707ca8f7439ed6c69b84b370fec2a72ab
CrazyOne <crazy@one.da>
2019-08-28 17:44:00 +0200

    Add Address model

---
+++ app/Address.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property int $student_id
+ * @property string $street_name
+ * @property string $street_number
+ * @property int $zip
+ * @property string $created_at
+ * @property string $updated_at
+ * @property Student $student
+ */
+class Address extends Model
+{
+    /**
+     * The table associated with the model.
+     * 
+     * @var string
+     */
+    protected $table = 'address';
+
+    /**
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function student()
+    {
+        return $this->belongsTo('App\Student');
+    }
+}
