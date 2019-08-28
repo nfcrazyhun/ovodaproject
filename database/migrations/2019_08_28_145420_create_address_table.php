0dce8121ac3847359a0d1ff3a056a7bbe04e5e20
CrazyOne <crazy@one.da>
2019-08-28 16:57:32 +0200

    Add migration: Create address table

---
+++ database/migrations/2019_08_28_145420_create_address_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('address', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('student_id');
+            $table->string('street_name');
+            $table->string('street_number');
+            $table->integer('zip');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('address');
+    }
+}
