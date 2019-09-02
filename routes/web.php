6712700a26d32fe8321c668f916391678380764e
CrazyOne <crazy@one.da>
2019-09-02 14:06:47 +0200

    Create url in menubar

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
-Route::get('/students', 'StudentController@index');
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/students', 'StudentController@index')->name('students.index');
