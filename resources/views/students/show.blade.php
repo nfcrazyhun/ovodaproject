934bb7833f673dafb6b59ea0f39def0d88bafbc1
CrazyOne <crazy@one.da>
2019-09-05 15:36:19 +0200

    Remove link

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -27,6 +27,5 @@
 
     <li><a href="{{ route('students.edit', ['id' => $student->id]) }}">Edit student</a></li>
     <li><a href="{{ route('addresses.edit', ['id' => $student->id]) }}">Edit address</a></li>
-    <li><a href="{{ route('addresses.create') }}">Add address</a></li>
 
 @stop
